cc3da1e9abac430262a1dbad54ab66e76bfa99a0
Sam Jewell <sam@bridge-u.com>
2017-02-26 16:51:12 +0000

    Pass the two failing tests that were just added - accept opened arrays…
    Or hashes that have no values in them at the end of the JSON string

--- lib/rjson/handler.rb
+++ lib/rjson/handler.rb
@@ -29,18 +29,20 @@ module RJSON
     end
 
     def result
-      # If we're in an unclosed array, remove any corrupted items from the
-      # end of the open array before processing:
+      # If we're in an unclosed array or hash and we have a corrupted token at
+      # the end of that context, remove that token
       last_tokens_context = @stack.last
       last_token = last_tokens_context.last
-      last_token_value = last_token.last
 
-      if last_token_value.is_a?(Numeric) || last_token_value == :corrupted
-        # Remove corrupted data
-        last_tokens_context.pop
+      if last_token.is_a?(Array)
+        last_token_value = last_token.last
+        if last_token_value.is_a?(Numeric) || last_token_value == :corrupted
+          # Remove corrupted data
+          last_tokens_context.pop
+        end
       end
 
-      # The next line succeeds whether or not all arrays have been ended already
+      # The next line succeeds whether or not all arrays/hashes have been closed
       root = @stack.first.last
       process root.first, root.drop(1)
     end
@@ -54,6 +56,7 @@ module RJSON
           rest.
           map { |x| process(x.first, x.drop(1)) }.
           each_slice(2).
+          # Only take Keys that also have values
           take_while { |i| i.size == 2 }
         ]
       when :scalar
